b9d10f3d2d1fd922b82ba6076103f35474b6b15b
matan <dev.matan@gmail.com>
2015-09-11 01:29:27 +0300

    cleanups, and avoid duplicate recording of member ownership

--- src/main/scala/extractor/plugin/TraversalExtraction.scala
+++ src/main/scala/extractor/plugin/TraversalExtraction.scala
@@ -4,6 +4,9 @@ import scala.tools.reflect.ToolBox
 import myUtil.Logging._
 
 /*
+ * TODO: turn this comment into an actionable such as logging the flags for forensics,
+ *       a ticket, documentation, or whatever. 
+ * 
  * Note: it is documented in the source, that 
  * a flag settings.Yshowsymowners, adds the symbol owner's id to the nameString.
  * In case this is so, owner chains may be obtained more cheaply. Also note that
@@ -13,11 +16,17 @@ import myUtil.Logging._
 object TraversalExtraction{
   
   def apply(global: Global)(unit: global.CompilationUnit) = {
-    import global._ // to have access to typed symbol methods
+    import global._ // for having access to typed symbol methods
     
-    // keep in mind we can also use the reflection 
-    // library's supplied Node.recordOwnerChain if it helps
+    /*
+     * Captures the node's hierarchy chain -  
+     * this is needed for the case that the node is a library symbol, 
+     * so we won't (necessarily) bump into its parents while compiling
+     * the project being compiled.  
+     */
     def recordOwnerChain(node: Node, symbol: Symbol): Unit = {
+      // Note: there is also the reflection library supplied Node.ownerChain method,
+      //       for now, the recursive iteration used here instead seems as good.
       if (!node.ownersTraversed)
       {
         if (symbol.nameString != "<root>") {
@@ -45,30 +54,15 @@ object TraversalExtraction{
     }
     
     
-    class ExtractAll(defParent: Option[global.Symbol]) extends Traverser {
+    class ExtractionTraversal(defParent: Option[global.Symbol]) extends Traverser {
       override def traverse(tree: Tree): Unit = {
         
         // see http://www.scala-lang.org/api/2.11.0/scala-reflect/index.html#scala.reflect.api.Trees 
         // for the different cases, as well as the source of the types matched against
         tree match {
           
-          // ignoring imports for now.. we get their symbols wherever they
-          // are used by the code, anyway.
-          case i @ Import(expr, selectors) =>
-            selectors.foreach {
-              case ImportSelector(nme.WILDCARD, _, null, _) => // a wildcard import
-                // in case of wildcard import we do not rely on any particular name being defined
-                // on `expr`; all symbols that are being used will get caught through selections
-                // println(tree)
-              case ImportSelector(name: Name, _, _, _) => // a specific import
-                def lookupImported(name: Name) = expr.symbol.info.member(name)
-                // importing a name means importing both a term and a type (if they exist)
-                // println(tree); println(lookupImported(name) + "(" + lookupImported(name).id + ")")
-            }
-
-          // captures member usage
+          // capture member usage
           case select: Select =>
-            println("Select at line" + select.pos.line)
             select.symbol.kindString match {
               case "constructor" => println(Console.MAGENTA + Console.BOLD + showRaw(tree) + Console.RESET)
               case "method" =>
@@ -101,7 +95,6 @@ object TraversalExtraction{
                 println("Processing select of kind " + select.symbol.kindString + " symbol: " + showRaw(select))
                 
                 if (defParent.isDefined) Edges(defParent.get.id, "uses", select.symbol.id)
-                println(defParent.getOrElse("root") + " uses: " + select.symbol + " (" + select.symbol.id + ")" + " of type " + select.symbol.tpe.typeSymbol)
                 
                 val node = Nodes(global)(select.symbol)
                 
@@ -109,59 +102,52 @@ object TraversalExtraction{
             }
             
           /*
-           * Idents are used in number of situations:
-           *  - to refer to local variable
-           *  - to refer to a top-level package (other packages are nested selections)
-           *  - to refer to a term defined in the same package as an enclosing class;
-           *    this looks fishy, see this thread:
+           *    See:
            *    https://groups.google.com/d/topic/scala-internals/Ms9WUAtokLo/discussion
+           *    https://groups.google.com/forum/#!topic/scala-internals/noaEpUb6uL4
            */
           case ident: Ident => println("ignoring Ident: " + ident.symbol)
-          case typeTree: TypeTree  => // are we missing something by not handling this?
-          
-          // Captures val definitions (rather than their automatic accessor methods..)
-          // This includes capturing the val's type (not kind) - the one that the val instantiates.
+            
+          // Capture val definitions (rather than their automatic accessor methods..)
           case ValDef(mods: Modifiers, name: TermName, tpt: Tree, rhs: Tree) =>
-            val s = tree.symbol
             
-            Nodes(global)(s)
-            Edges(defParent.get.id, "declares member", s.id)
+            val symbol = tree.symbol
             
-            val valueType = s.tpe.typeSymbol // the type that this val instantiates.
+            Nodes(global)(symbol)
+            Edges(defParent.get.id, "declares member", symbol.id)
+            
+            // Capturing the defined val's type (not kind) while at it
+            val valueType = symbol.tpe.typeSymbol // the type that this val instantiates.
             val node = Nodes(global)(valueType)
             recordOwnerChain(node, valueType)
             
-            Edges(s.id, "is of type", valueType.id)
-            
-            println(defParent.get.id + " has own value: " + s.kindString + " " + s.nameString + " (" + s.id + ") of type " + valueType.id)
+            Edges(symbol.id, "is of type", valueType.id)
             
           // Capture defs of methods.
           // Note this will also capture default constructors synthesized by the compiler
           // and synthetic accessor methods defined by the compiler for vals
           case DefDef(mods, name, tparams, vparamss, tpt, rhs) =>
-            val s = tree.symbol
+            val symbol = tree.symbol
             
-            Nodes(global)(s)
-            Edges(defParent.get.id, "declares member", s.id)
-            
-            println(defParent.get.id + " declares own member: " + s.kindString + " " + s.nameString + " (" + s.id + ")")
-            
-            val traverser = new ExtractAll(Some(tree.symbol))
-            if (s.nameString == "get"){             
+            Nodes(global)(symbol)
+            Edges(defParent.get.id, "declares member", symbol.id)
+                       
+            val traverser = new ExtractionTraversal(Some(tree.symbol))
+            if (symbol.nameString == "get"){             
               val tracer = new TraceTree
               tracer.traverse(tree)
               println(Console.RED + Console.BOLD + showRaw(rhs))
-              println(s.tpe.typeSymbol)
+              println(symbol.tpe.typeSymbol)
             }
             traverser.traverse(rhs)
           
-          // Capture type definition (classes, traits, objects)
+          // Capture type definitions (classes, traits, objects)
           case Template(parents, self, body) =>
 
-            val ts = tree.tpe.typeSymbol
+            val typeSymbol = tree.tpe.typeSymbol
                         
-            val node = Nodes(global)(ts)
-            recordOwnerChain(node, ts)
+            val node = Nodes(global)(typeSymbol)
+            recordOwnerChain(node, typeSymbol)
             
             val parentTypeSymbols = parents.map(parent => parent.tpe.typeSymbol).toSet
             parentTypeSymbols.foreach { s => 
@@ -169,42 +155,31 @@ object TraversalExtraction{
               recordOwnerChain(parentNode, s)
             }
 
-            if (defParent.isDefined) Edges(ts.id, "owned by", defParent.get.id) // TODO: move up for readability
-              
-            parentTypeSymbols.foreach(s => 
-              Edges(ts.id, "extends", s.id))
-              
-
-            println
-            println(tree.tpe.typeSymbol.kindString + " " + tree.tpe.typeSymbol.nameString + " (" + tree.tpe.typeSymbol.id + ") ")
-            //sourceSpan(tree)
-            
-            //parentTypeSymbols.foreach(s => println("extends: " + s.kindString + " " + s.nameString + " (" + s.id + ") "))
-            
-            //tree.tpe.declarations.foreach(s => println("declares own member: " + s.kindString + " " + s.nameString + " (" + s.id + ")")) // TODO: need to deduplicate these
-            
-            val traverser = new ExtractAll(Some(tree.tpe.typeSymbol))
-            body foreach { tree =>
-              traverser.traverse(tree)
+            if (defParent.isDefined) {
+              if (defParent.get.id != typeSymbol.owner.id) println(Console.YELLOW + "parent not owner")              
             }
+                          
+            parentTypeSymbols.foreach(s => 
+              Edges(typeSymbol.id, "extends", s.id))
+              
+            val traverser = new ExtractionTraversal(Some(tree.tpe.typeSymbol))
+            body foreach { tree => traverser.traverse(tree) }
             
           case tree =>
-            super.traverse(tree) // println(" =========> general traverse call ")
+            super.traverse(tree) 
          
         }        
       }
     }
         
-    val traverser = new ExtractAll(None)
+    val traverser = new ExtractionTraversal(None)
     traverser.traverse(unit.body)
     
     println
-    //Nodes.list.map(node => println(node._2))
     println("total " + Nodes.list.size + " nodes")
-    //Edges.list.map(println)
     println("total " + Edges.list.size + " edges")
     Output.write
     
-    Unit // this statement guards against a compiler crash
+    Unit // Should return Unit
   }
 }
